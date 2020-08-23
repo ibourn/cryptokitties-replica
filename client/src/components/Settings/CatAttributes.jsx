6778440ef01bb6a3dc0bbc84d791b9a5b0e71a84
ibourn <36422029+ibourn@users.noreply.github.com>
2020-08-23 04:05:07 +0200

    style cattributes

--- client/src/components/Settings/CatAttributes.jsx
+++ client/src/components/Settings/CatAttributes.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import InputRange from './InputRange';
 
 import { allEyeVariations } from '../../assets/modules/eyevariations';
@@ -24,15 +27,25 @@ export default function CatAttributes(props) {
             <InputRange text="Eyes shape" min="1" max="7"
                 item='eyesShape' dna={props.dna} handleChange={props.handleChange}>
             </InputRange>
-            <InputRange text="Decorative Pattern" min="1" max="7"
+            <InputRange text="Decoration Pattern" min="1" max="7"
                 item='decorationPattern' dna={props.dna} handleChange={props.handleChange}>
             </InputRange>
-            <InputRange text="Pattern color 1" min="10" max="98"
-                item='decorationMidcolor' dna={props.dna} handleChange={props.handleChange}>
-            </InputRange>
-            <InputRange text="Pattern color 2" min="10" max="98"
-                item='decorationSidescolor' dna={props.dna} handleChange={props.handleChange}>
-            </InputRange>
+            <span style={{ fontWeight: 'bold'}}>
+                Decoration color pattern:
+             </span>
+            <Row noGutters style={{ marginTop: '0.7em' }}>
+                <Col lg={5}>
+                    <InputRange text="Main color" min="10" max="98"
+                        item='decorationMidcolor' dna={props.dna} handleChange={props.handleChange}>
+                    </InputRange>
+                </Col>
+                <Col lg={2}></Col>
+                <Col lg={5}>
+                    <InputRange text="Sides color" min="10" max="98"
+                        item='decorationSidescolor' dna={props.dna} handleChange={props.handleChange}>
+                    </InputRange>
+                </Col>
+            </Row>
             <InputRange text="Animation" min="1" max="7"
                 item='animation' dna={props.dna} handleChange={props.handleChange}>
             </InputRange>
@@ -46,4 +59,4 @@ export default function CatAttributes(props) {
 CatAttributes.propTypes = {
     dna: PropTypes.object,
     handleChange: PropTypes.func,
-  };
+};
